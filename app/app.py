c5af29667aa981c0ba39fb14df4859efe00ce3ae
Priyansh <saxenapriyansh95@gmail.com>
2026-09-14 11:11:24 +0530

    drafted the initial structure of the app

--- app/app.py
+++ app/app.py
@@ -1,2 +1,32 @@
 import streamlit as st 
+import pandas as pd 
+import numpy as np 
+import plotly.graph_objects as go 
+from plotly.subplots import make_subplots
 
+
+# ========================================================================================= #
+
+
+# OHLCV downloader 
+# the function download_data(ticker) should effectively download the raw csv data 
+# it needs to be stripped off of input() and print() for it to work flawlessly 
+
+
+
+
+
+# tick inspector 
+# in this case the tick_inspector function should do exactly the same as above 
+
+
+
+
+# quant_analyzer should do same as above 
+
+
+
+# user interface 
+# build a control panel on the left side of the screen 
+# here the user should be able to type their ticker or ideally choose from a dropdown menu 
+# execution should happen based on checking if st.sidebar.button 
